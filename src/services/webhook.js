6ba7f49f3fbdafc85401f8c4eeb11eeecbb0ae77
guuibayer <guiilherme.bayer@gmail.com>
2016-12-11 07:30:50 +0000

    b992207 Add map to loop messages from bot

--- src/services/webhook.js
+++ src/services/webhook.js
@@ -29,12 +29,12 @@ var WebhookService = function () {
 
   _createClass(WebhookService, [{
     key: 'textMessage',
-    value: function textMessage(sender, text, token) {
+    value: function textMessage(sender, text) {
       var messageData = { text: text };
 
       (0, _request2.default)({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
+        qs: { access_token: _index2.default.FACEBOOK_PAGE_ACCESS_TOKEN },
         method: 'POST',
         json: {
           recipient: { id: sender },
@@ -63,9 +63,14 @@ var WebhookService = function () {
   }, {
     key: 'messageEvent',
     value: function messageEvent(req, res) {
-      // let messagingEvents = req.body.entry[0].messaging;
+      var messagingEvents = req.body.entry[0].messaging;
 
       console.log(req.body);
+
+      messagingEvents.map(function (messagingEvent) {
+        console.log(messagingEvent);
+      });
+
       /*
         for (let i = 0; i < messagingEvents.length; i++) {
           let event = req.body.entry[0].messaging[i];
@@ -73,11 +78,11 @@ var WebhookService = function () {
           
           if (event.message && event.message.text) {
             let text = event.message.text;
-            WebhookService.sendTextMessage(sender, `Text received, echo: ${text.substring(0, 200)}`, config.FACEBOOK_PAGE_ACCESS_TOKEN);
+            WebhookService.sendTextMessage(sender, `Text received, echo: ${text.substring(0, 200)}`);
           }
            if (event.postback) {
             let text = JSON.stringify(event.postback);
-             WebhookService.textMessage(sender, `Postback received: ${text.substring(0, 200)}`, config.FACEBOOK_PAGE_ACCESS_TOKEN);
+             WebhookService.textMessage(sender, `Postback received: ${text.substring(0, 200)}`);
             continue;
           }
         }*/
