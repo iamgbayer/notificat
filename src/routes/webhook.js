eecbc6a17256078766e552dc96327d63569147d3
guuibayer <guiilherme.bayer@gmail.com>
2016-12-11 01:35:23 -0200

    96ee859 Fix in deploy task

---
+++ src/routes/webhook.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _webhook = require('../services/webhook');
+
+var _webhook2 = _interopRequireDefault(_webhook);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var router = _express2.default.Router();
+
+// router.get('/', WebhookService.oi);
+
+exports.default = router;
+module.exports = exports['default'];
+//# sourceMappingURL=webhook.js.map
