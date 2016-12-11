13b2c88a53a25e0989359c1399601d977e0f0b2c
guuibayer <guiilherme.bayer@gmail.com>
2016-12-11 05:44:23 -0200

    Corrections in textMessage() call

--- src/services/webhook.js
+++ src/services/webhook.js
@@ -19,40 +19,23 @@ class WebhookService {
     console.log(req.body)
 
     messagingEvents.map(messagingEvent => {
-      console.log('message', messagingEvent.message.text ,' sender', messagingEvent.sender.id)
+      let message = messagingEvent.message.text;
+      let sender = messagingEvent.sender.id;
+
+      WebhookService.textMessage(sender, message.substring(0, 200));
     })
 
-  /*
-    for (let i = 0; i < messagingEvents.length; i++) {
-      let event = req.body.entry[0].messaging[i];
-      let sender = event.sender.id;
-      
-      if (event.message && event.message.text) {
-        let text = event.message.text;
-        WebhookService.sendTextMessage(sender, `Text received, echo: ${text.substring(0, 200)}`);
-      }
-
-      if (event.postback) {
-        let text = JSON.stringify(event.postback);
-
-        WebhookService.textMessage(sender, `Postback received: ${text.substring(0, 200)}`);
-        continue;
-      }
-    }*/
-
-    // res.sendStatus(200);
+    res.sendStatus(200);
   }
 
-  textMessage (sender, text) {
-    let messageData = { text: text }
-
+  static textMessage (sender, message) {
     request({
       url: 'https://graph.facebook.com/v2.6/me/messages',
       qs: { access_token: config.FACEBOOK_PAGE_ACCESS_TOKEN },
       method: 'POST',
       json: {
         recipient: { id: sender },
-        message: messageData,
+        message: { text: message },
       }
     }, (error, response, body) => {
       if (error) {
